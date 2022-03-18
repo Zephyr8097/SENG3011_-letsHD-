5e6cc8bdf1c67bcc5c73950055b5cc68fff77b85
lavine66 <849217890@qq.com>
2022-03-18 17:15:03 +0800

    host 0000

--- PHASE_1/API_SourceCode/flaskr/app.py
+++ PHASE_1/API_SourceCode/flaskr/app.py
@@ -239,7 +239,7 @@ class Reports(Resource):
 api.add_resource(Reports, "/reports")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8080)
 
 
 
